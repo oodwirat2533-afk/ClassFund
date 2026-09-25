74db758ee48820e1a8166ee7b53b91360b28f056
wirat <ood.wirat2533@gmail.com>
2026-09-25 19:42:52 +0700

    Clean up temporary fix script

--- fix.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let lines = fs.readFileSync('js/app.js', 'utf8').split(/\r?\n/);
-for(let i=0; i<lines.length; i++) {
-  if (lines[i].includes('const fullClassStr = ${clsName} ภาคเรียนที่ /;')) {
-    lines[i] = '      const fullClassStr = `${clsName} ภาคเรียนที่ ${semStr}/${yrStr}`;';
-  }
-}
-fs.writeFileSync('js/app.js', lines.join('\n'));
-console.log('Fixed fullClassStr');
